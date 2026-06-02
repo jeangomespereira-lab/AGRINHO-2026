ef52a9a631ee4e239cf2291d98fd0a618fcea489
jeangomespereira-lab <jean.gomes.pereira@escola.pr.gov.br>
2026-06-02 20:11:58 -0300

    @jeangomespereira-lab

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Seleciona o botão e o texto de mensagem do HTML
+const botao = document.getElementById('meuBotao');
+const mensagem = document.getElementById('mensagem');
 
+// Adiciona um evento que "escuta" quando o botão é clicado
+botao.addEventListener('click', function() {
+    // Quando clicado, ele muda o estilo da mensagem para aparecer na tela
+    if (mensagem.style.display === 'block') {
+        mensagem.style.display = 'none'; // Esconde se já estiver aparecendo
+    } else {
+        mensagem.style.display = 'block'; // Mostra a mensagem
+    }
+});
